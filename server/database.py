d4b868c34bb7699de7008b80a3d41c8190729a11
tmznwnel00 <tmznwnel00@gmail.com>
2023-10-21 00:23:08 +0900

    feat: var to .env

--- server/database.py
+++ server/database.py
@@ -1,9 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-user = 'test'
-password = 'Abc1234!'
+user = os.getenv('USER')
+password = os.getenv('PASSWORD')
 SQLALCHEMY_DATABASE_URL = f"mysql://{user}:{password}@localhost/datedive"
 
 engine = create_engine(

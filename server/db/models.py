93cfab5f7ca80f1fb6237c4d33abd5d2f36cc76a
sungminoh <smoh2044@gmail.com>
2023-11-06 16:27:02 +0900

    user `<table>_id` as primary key for smarter foreign key join

--- server/db/models.py
+++ server/db/models.py
@@ -18,46 +18,47 @@ class SoftDeletable:
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, autoincrement=True, primary_key=True)
+    user_id = Column(Integer, autoincrement=True, primary_key=True)
     email = Column(String(100), nullable=False, unique=True, index=True)
     password = Column(String(100), nullable=False)
     verified = Column(Boolean, nullable=False, default=False)
-    nickname = Column(String(15), nullable=False, unique=True)
-    gender = Column(Enum('male', 'female'), nullable=False)
-    age = Column(Integer, nullable=False)
+    nickname = Column(String(15), unique=True)
+    gender = Column(Enum('male', 'female'))
+    age = Column(Integer)
     location = Column(String(100))
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     last_login = Column(DateTime(timezone=True), onupdate=func.now())
     # relationships
-    hashtags = relationship("Hashtag", secondary="user_hashtags")
-    question_answers = relationship("UserQuestionAnswer", foreign_keys='user_question_answer.user_id')
-    photos = relationship("UserPhoto", foreign_keys='user_photo.user_id')
+    hashtags = relationship('Hashtag', secondary='user_hashtag')
+    question_answers = relationship("UserQuestionAnswer")
+    photos = relationship("UserPhoto")
 
 
 class Hashtag(Base):
     __tablename__ = 'hashtag'
 
-    id = Column(Integer, autoincrement=True, primary_key=True)
+    hashtag_id = Column(Integer, autoincrement=True, primary_key=True)
     type = Column(String(50))  # sport/music/food/pet/etc.
     name = Column(String(100), nullable=False, unique=True)  # snowboard/rock/vegan/etc.
 
 
 class UserHashtag(Base, SoftDeletable):
-    __tablename__ = 'user_hashtags'
+    __tablename__ = 'user_hashtag'
 
-    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    hashtag_id = Column(Integer, ForeignKey('hashtag.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
+    hashtag_id = Column(Integer, ForeignKey('hashtag.hashtag_id'), primary_key=True)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     deleted_at = Column(DateTime)  # null if not deleted
+    # relationships
 
 
 class UserPhoto(Base, SoftDeletable):
     __tablename__ = "user_photo"
 
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    user_id = Column(Integer, nullable=False, index=True)
+    user_photo_id = Column(Integer, autoincrement=True, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
     s3_path = Column(String(500), nullable=False)
     description = Column(Text)
     order_index = Column(Integer, nullable=False)
@@ -69,9 +70,9 @@ class UserPhoto(Base, SoftDeletable):
 class MatchStatus(Base):
     __tablename__ = "match_status"
 
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    swiper_id = Column(Integer, ForeignKey('user.id'))
-    swiped_id = Column(Integer, ForeignKey('user.id'))
+    match_id = Column(Integer, autoincrement=True, primary_key=True)
+    swiper_id = Column(Integer, ForeignKey('user.user_id'))
+    swiped_id = Column(Integer, ForeignKey('user.user_id'))
     status = Column(Enum('view', 'pass', 'wait', 'rejected', 'match', 'broken'), nullable=False)
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
@@ -80,7 +81,7 @@ class MatchStatus(Base):
 class Question(Base):
     __tablename__ = "question"
 
-    id = Column(Integer, primary_key=True)
+    question_id = Column(Integer, primary_key=True)
     description = Column(Text, nullable=False)
     options = Column(JSON)  # null for text type answer
     multi_allowed = Column(Boolean)
@@ -91,12 +92,12 @@ class Question(Base):
 class UserQuestionAnswer(Base, SoftDeletable):
     __tablename__ = "user_question_answer"
 
-    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
-    question_id = Column(Integer, ForeignKey('question.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'), primary_key=True)
+    question_id = Column(Integer, ForeignKey('question.question_id'), primary_key=True)
     order_index = Column(Integer, nullable=False)
     answer = Column(Text)  # key of question.options or text type answer
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     updated_at = Column(DateTime(timezone=True), onupdate=func.now())
     deleted_at = Column(DateTime)
     # relationship
-    question = relationship("Question", foreign_keys='question.id')
+    question = relationship("Question")

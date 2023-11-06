dc3da8f9708aa88c35de137dda148b1c6a312c00
sungminoh <smoh2044@gmail.com>
2023-11-06 15:26:21 +0900

    refactor porject structure and change db schema

---
+++ server/db/models.py
@@ -0,0 +1,102 @@
+from datetime import datetime
+
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, Enum, \
+    DateTime, JSON, func, Text
+from sqlalchemy.orm import relationship, object_session
+
+from .base import Base
+
+
+
+class SoftDeletable:
+    def soft_delete(self):
+        self.deleted_at = datetime.utcnow()
+        session = object_session(self)
+        session.add(self)
+
+
+class User(Base):
+    __tablename__ = "user"
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    email = Column(String(100), nullable=False, unique=True, index=True)
+    password = Column(String(100), nullable=False)
+    verified = Column(Boolean, nullable=False, default=False)
+    nickname = Column(String(15), nullable=False, unique=True)
+    gender = Column(Enum('male', 'female'), nullable=False)
+    age = Column(Integer, nullable=False)
+    location = Column(String(100))
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    last_login = Column(DateTime(timezone=True), onupdate=func.now())
+    # relationships
+    hashtags = relationship("Hashtag", secondary="user_hashtags")
+    question_answers = relationship("UserQuestionAnswer", foreign_keys='user_question_answer.user_id')
+    photos = relationship("UserPhoto", foreign_keys='user_photo.user_id')
+
+
+class Hashtag(Base):
+    __tablename__ = 'hashtag'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    type = Column(String(50))  # sport/music/food/pet/etc.
+    name = Column(String(100), nullable=False, unique=True)  # snowboard/rock/vegan/etc.
+
+
+class UserHashtag(Base, SoftDeletable):
+    __tablename__ = 'user_hashtags'
+
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    hashtag_id = Column(Integer, ForeignKey('hashtag.id'), primary_key=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    deleted_at = Column(DateTime)  # null if not deleted
+
+
+class UserPhoto(Base, SoftDeletable):
+    __tablename__ = "user_photo"
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    user_id = Column(Integer, nullable=False, index=True)
+    s3_path = Column(String(500), nullable=False)
+    description = Column(Text)
+    order_index = Column(Integer, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    deleted_at = Column(DateTime)  # null if not deleted
+
+
+class MatchStatus(Base):
+    __tablename__ = "match_status"
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    swiper_id = Column(Integer, ForeignKey('user.id'))
+    swiped_id = Column(Integer, ForeignKey('user.id'))
+    status = Column(Enum('view', 'pass', 'wait', 'rejected', 'match', 'broken'), nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class Question(Base):
+    __tablename__ = "question"
+
+    id = Column(Integer, primary_key=True)
+    description = Column(Text, nullable=False)
+    options = Column(JSON)  # null for text type answer
+    multi_allowed = Column(Boolean)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+
+class UserQuestionAnswer(Base, SoftDeletable):
+    __tablename__ = "user_question_answer"
+
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
+    question_id = Column(Integer, ForeignKey('question.id'), primary_key=True)
+    order_index = Column(Integer, nullable=False)
+    answer = Column(Text)  # key of question.options or text type answer
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+    deleted_at = Column(DateTime)
+    # relationship
+    question = relationship("Question", foreign_keys='question.id')

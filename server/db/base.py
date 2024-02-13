d38e6f6a9918133e14f2d2f74913699cdf6b1cc0
tmznwnel00 <tmznwnel00@gmail.com>
2024-02-13 23:45:00 +0900

    feat: import module order

--- server/db/base.py
+++ server/db/base.py
@@ -1,8 +1,9 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-import os
-from dotenv import load_dotenv
 
 load_dotenv()
 user = os.getenv('DB_USER')
